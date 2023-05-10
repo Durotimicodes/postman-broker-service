835bdad5df35d61c4cdb44689dc674f6ca80053c
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-05-10 03:43:50 +0100

    new functions

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -5,11 +5,7 @@ import (
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
+
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
@@ -18,9 +14,7 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
-
+	- = app.writeJSON(w, httpStatusOK, paypayload){
+		
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module broker
 go 1.20
 
 require (
+	github.com/brianvoe/gofakeit/v6 v6.21.0
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
 )
